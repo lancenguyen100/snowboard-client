3c83e796880a2c89d12b62f3a13ed81f2c85bb6f
tinhi3 <lancetnguyen100@gmail.com>
2022-07-31 11:45:16 -0400

    set up readme and rename app

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -2,7 +2,8 @@ let apiUrl
 const apiUrls = {
     // YOU MUST CHANGE PRODUCTION URL WHEN DEPLOYING
 	production: '<replace_with_deployed_api_url>',
-	development: 'http://localhost:8000',
+	// development: 'http://localhost:8000',
+	development: 'http://127.0.0.1:8000',
 }
 
 if (window.location.hostname === 'localhost') {
